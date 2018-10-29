5dc57d0a0e7fd4efeaa6be638af18b0b0691e3ec
mcclowes <maxcc@me.com>
2018-10-29 13:05:33 +0000

    absolute-path works

--- src/index.js
+++ src/index.js
@@ -1,52 +1,5 @@
 export const rules = {
-  'no-unresolved': require('./rules/no-unresolved'),
-  'named': require('./rules/named'),
-  'default': require('./rules/default'),
-  'namespace': require('./rules/namespace'),
-  'no-namespace': require('./rules/no-namespace'),
-  'export': require('./rules/export'),
-  'no-mutable-exports': require('./rules/no-mutable-exports'),
-  'extensions': require('./rules/extensions'),
-  'no-restricted-paths': require('./rules/no-restricted-paths'),
-  'no-internal-modules': require('./rules/no-internal-modules'),
-  'group-exports': require('./rules/group-exports'),
-  'no-relative-parent-imports': require('./rules/no-relative-parent-imports'),
-
-  'no-self-import': require('./rules/no-self-import'),
-  'no-cycle': require('./rules/no-cycle'),
-  'no-named-default': require('./rules/no-named-default'),
-  'no-named-as-default': require('./rules/no-named-as-default'),
-  'no-named-as-default-member': require('./rules/no-named-as-default-member'),
-  'no-anonymous-default-export': require('./rules/no-anonymous-default-export'),
-
-  'no-commonjs': require('./rules/no-commonjs'),
-  'no-amd': require('./rules/no-amd'),
-  'no-duplicates': require('./rules/no-duplicates'),
-  'first': require('./rules/first'),
-  'max-dependencies': require('./rules/max-dependencies'),
-  'no-extraneous-dependencies': require('./rules/no-extraneous-dependencies'),
-  'no-absolute-path': require('./rules/no-absolute-path'),
-  'no-nodejs-modules': require('./rules/no-nodejs-modules'),
-  'no-webpack-loader-syntax': require('./rules/no-webpack-loader-syntax'),
-  'order': require('./rules/order'),
-  'newline-after-import': require('./rules/newline-after-import'),
-  'prefer-default-export': require('./rules/prefer-default-export'),
-  'no-default-export': require('./rules/no-default-export'),
-  'no-named-export': require('./rules/no-named-export'),
-  'no-dynamic-require': require('./rules/no-dynamic-require'),
-  'unambiguous': require('./rules/unambiguous'),
-  'no-unassigned-import': require('./rules/no-unassigned-import'),
-  'no-useless-path-segments': require('./rules/no-useless-path-segments'),
-  'dynamic-import-chunkname': require('./rules/dynamic-import-chunkname'),
-
-  // export
-  'exports-last': require('./rules/exports-last'),
-
-  // metadata-based
-  'no-deprecated': require('./rules/no-deprecated'),
-
-  // deprecated aliases to rules
-  'imports-first': require('./rules/imports-first'),
+  'absolute-path': require('./rules/absolute-path'),
 }
 
 export const configs = {
